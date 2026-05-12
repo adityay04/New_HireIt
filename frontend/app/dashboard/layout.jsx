3b2a7f12b2a3aa4eba2594e32213dba00e57ee77
Aditya Yadav <adityayadav0304@gmail.com>
2026-05-12 18:42:28 +0530

    fixed layout.jsx

--- frontend/app/dashboard/layout.jsx
+++ frontend/app/dashboard/layout.jsx
@@ -1,34 +1,19 @@
-"use client";
+import "./globals.css";
 
-import { useEffect } from "react";
+export const metadata = {
+  title: "HireIt",
+  description: "AI Resume Builder",
+};
 
-import { useRouter } from "next/navigation";
-
-import Sidebar from "@/components/Sidebar";
-
-export default function DashboardLayout({
+export default function RootLayout({
   children,
 }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const token =
-      localStorage.getItem("token");
-
-    if (!token) {
-      router.push("/login");
-    }
-  }, []);
-
   return (
-    <div className="flex bg-black text-white">
-
-      <Sidebar />
-
-      <main className="flex-1 p-8">
-        {children}
-      </main>
-
-    </div>
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
+      <body>{children}</body>
+    </html>
   );
 }
